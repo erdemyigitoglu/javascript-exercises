428e4baad3d4f6758a0ec197bce1e1e878fdf32e
Erdem Yigitoglu <erdemyigitoglu@gmail.com>
2020-11-16 16:33:38 +0300

    try for sumAll

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+let sum = 0;
+if ((typeof a == "number" || typeof b != "number") || (a < 0 || b< 0))  {
+sum = "ERROR";
+} else if (a < b) {
+for (let num = a; num <= b; num++) {
+sum += num;
+}} else {
+for (let num = b; num <= a; num++) {
+sum += num;
+}}
+return sum;
 }
 
 module.exports = sumAll
