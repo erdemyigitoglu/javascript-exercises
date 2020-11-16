5be1829857088b9ac0f884336e7519008ec88054
Erdem Yigitoglu <erdemyigitoglu@gmail.com>
2020-11-16 17:54:06 +0300

    finished tempConversion

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(f) {
+let c = (f - 32)/1.8;
+return +c.toFixed(1);
 }
 
-const ctof = function() {
-
+const ctof = function(c) {
+let f = 1.8*c + 32;
+return +f.toFixed(1)
 }
 
 module.exports = {
